5327bb89209271b7aaadb048185e2ded6a94fb4f
Kiren Paul <kirenpaul@gmail.com>
2024-09-02 13:42:27 +0700

    Update executeNativeFunction.js

--- injectedJavaScript/executeNativeFunction.js
+++ injectedJavaScript/executeNativeFunction.js
@@ -1,4 +1,4 @@
-var content = `
+const content = `
   function executeNativeFunction(fnName, args) {
     window.location.hash = '&executeFunction<-' + fnName + '&' + '&arguments<-' + JSON.stringify(args) + '&' + window.ReactNativeWebView.postMessage(JSON.stringify(args)) + '&';
   }
